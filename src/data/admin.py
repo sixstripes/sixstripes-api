36908b6d8d6cc8b736f155e73a39ea16c126712a
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2019-12-22 20:20:33 -0300

    Add csv importer mixin

--- src/data/admin.py
+++ src/data/admin.py
@@ -1,15 +1,12 @@
 from data.models import Occupation, Politician, SexualOrientation
-from django.contrib import admin, messages
-from django.http import HttpResponseNotAllowed
-from django.shortcuts import redirect
-from django.urls import path
+from django.contrib import admin
 
-from .forms import CsvImportForm
+from .mixins import ImportCSVAdminMixin
 from .translator import PoliticianDataTranslator
 
 
 @admin.register(Politician)
-class PoliticianAdmin(admin.ModelAdmin):
+class PoliticianAdmin(ImportCSVAdminMixin, admin.ModelAdmin):
     change_list_template = "politician_changelist.html"
     list_display = (
         "name",
@@ -17,38 +14,9 @@ class PoliticianAdmin(admin.ModelAdmin):
         "country",
         "reference",
     )
-
-    class Media:
-        js = ("//code.jquery.com/jquery-3.4.1.slim.min.js", "js/admin-csv-import.js")
-        css = {"all": ("css/admin-csv-import.css",)}
-
-    def get_urls(self):
-        urls = super().get_urls()
-        my_urls = [
-            path("import/", self.import_csv, name="data_politician_import_csv"),
-        ]
-        return my_urls + urls
-
-    def changelist_view(self, request, extra_context=None):
-        extra_context = extra_context or {}
-        extra_context["form"] = CsvImportForm()
-        return super().changelist_view(request, extra_context=extra_context)
-
-    def import_csv(self, request):
-        if request.method != "POST":
-            return HttpResponseNotAllowed(permitted_methods=["POST"])
-
-        csv_file = request.FILES["csv_file"]
-        if csv_file.content_type != "text/csv":
-            self.message_user(request, "Invalid file format", level=messages.ERROR)
-            return redirect("admin:data_politician_changelist")
-
-        data_translator = PoliticianDataTranslator(csv_file)
-        data_translator.parse_rows()
-        data_translator.to_objects_list()
-
-        self.message_user(request, "Your csv file has been imported")
-        return redirect("admin:data_politician_changelist")
+    translator_class = PoliticianDataTranslator
+    import_csv_url_name = "data_politician_import_csv"
+    import_csv_redirect_url_name = "admin:data_politician_changelist"
 
 
 @admin.register(Occupation)
--- src/data/mixins.py
+++ src/data/mixins.py
@@ -1,6 +1,12 @@
+from django.contrib import messages
 from django.db import models
+from django.http import HttpResponseNotAllowed
+from django.shortcuts import redirect
+from django.urls import path
 from django.utils.translation import gettext_lazy as _
 from django_countries.fields import CountryField
+
+from .forms import CsvImportForm
 
 
 class PersonMixin(models.Model):
@@ -14,3 +20,37 @@ class PersonMixin(models.Model):
 
     class Meta:
         abstract = True
+
+
+class ImportCSVAdminMixin:
+    class Media:
+        js = ("//code.jquery.com/jquery-3.4.1.slim.min.js", "js/admin-csv-import.js")
+        css = {"all": ("css/admin-csv-import.css",)}
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path("import/", self.import_csv, name=self.import_csv_url_name),
+        ]
+        return my_urls + urls
+
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context["form"] = CsvImportForm()
+        return super().changelist_view(request, extra_context=extra_context)
+
+    def import_csv(self, request):
+        if request.method != "POST":
+            return HttpResponseNotAllowed(permitted_methods=["POST"])
+
+        csv_file = request.FILES["csv_file"]
+        if csv_file.content_type != "text/csv":
+            self.message_user(request, "Invalid file format", level=messages.ERROR)
+            return redirect(self.import_csv_redirect_url_name)
+
+        data_translator = self.translator_class(csv_file)
+        data_translator.parse_rows()
+        data_translator.to_objects_list()
+
+        self.message_user(request, "Your csv file has been imported")
+        return redirect(self.import_csv_redirect_url_name)
