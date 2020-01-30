5a2c7541da8810ee2dd5111cb294a4c35787a06a
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2020-01-30 00:13:14 -0300

    Add authentication

--- src/sixstripes_api/settings.py
+++ src/sixstripes_api/settings.py
@@ -128,6 +128,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ],
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 20,
