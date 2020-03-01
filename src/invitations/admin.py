7b00dc138e9052619be10d3c4168f48bd3e2dbc8
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2020-03-01 00:05:31 -0300

    Send email on invite arppoved

--- src/invitations/admin.py
+++ src/invitations/admin.py
@@ -1,8 +1,10 @@
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.utils.crypto import get_random_string
 from invitations import models
 from rest_framework.authtoken.models import Token
+from templated_email import send_templated_mail
 
 
 @admin.register(models.Invite)
@@ -19,19 +21,25 @@ class InviteAdmin(admin.ModelAdmin):
     actions = ["approve_selected_invites", "reject_selected_invites"]
 
     def approve_selected_invites(self, request, queryset):
-        for invite in queryset:
+        for invite in queryset.exclude(status="approved"):
             user = User.objects.create(
                 username=get_random_string(length=150),
                 first_name=invite.name,
                 email=invite.email,
                 is_active=True,
             )
-            Token.objects.create(user=user)
+            token = Token.objects.create(user=user)
+            send_templated_mail(
+                template_name="invite_approved",
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=[invite.email],
+                context={"invite": invite, "token": token},
+            )
 
         queryset.update(status="approved")
 
     def reject_selected_invites(self, request, queryset):
-        for invite in queryset:
+        for invite in queryset.exclude(status="rejected"):
             users = User.objects.filter(first_name=invite.name, email=invite.email)
             users.update(is_active=False)
             Token.objects.filter(user__in=users).delete()
