b0d8b5caaa6eb15c4b9f1a2ad28d1949fcab9577
Petterson Alves <pettalves@gmail.com>
2020-05-03 22:15:21 -0300

    Add not found message

--- src/static/modules/app.js
+++ src/static/modules/app.js
@@ -1,12 +1,32 @@
 function app() {
   const search = document.querySelector(".search-input");
-
+  
   function update() {
-    console.log("foi");
+    const searchChild = document.querySelector('div[data-role="search:results"]');
+    const notFound = document.querySelector('.not-found');
+    
+    if (search.value.length > 3 && !searchChild && !notFound) {
+      const tag = document.createElement("p");
+      const text = document.createTextNode("Não encontrado");
+      tag.appendChild(text);
+      
+      search.parentNode.appendChild(tag).classList.add("not-found");
+    }
+
+    if (search.value.length < 4 && notFound) {
+      notFound.parentNode.removeChild(notFound);
+    }
+
+    if (notFound) {
+      const closeSearch = document.querySelector('.menu-content > div > i');
+
+      closeSearch.addEventListener("click", function () {
+        notFound.parentNode.removeChild(notFound);
+      });
+    }
   }
 
-  search.addEventListener("change", update);
-  console.log("caceta")
+  search.addEventListener("keyup", update);
 }
 
 window.app = app;
